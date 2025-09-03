2ef8496714ee62279adbe9ea0f7474b8d3e78de9
Omar Louis White <louiswhite3019@icloud.com>
2025-09-02 21:13:38 -0400

    fix: runs linter

--- collections/Media.ts
+++ collections/Media.ts
@@ -1,6 +1,6 @@
-import type { CollectionConfig } from "payload";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import type { CollectionConfig } from "payload";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
