3cef280d0cd4cb0e6aee5f6ba5477b4951759ec2
Omar Louis White <louiswhite3019@icloud.com>
2025-11-30 20:31:10 -0500

    chore: adds comment

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,6 @@
 import type { MDXComponents } from "mdx/types";
 
+//This is where I would override what gets rendered for different markdown elements
 const components: MDXComponents = {};
 
 export function useMDXComponents(): MDXComponents {
