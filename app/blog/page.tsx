7859b837d1b7aaf7e6b35d659784d9c6e9e85943
Omar Louis White <louiswhite3019@icloud.com>
2025-12-04 00:24:22 -0500

    convert some pages back to regular next pages

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import AllBlogPosts from "@/components/blog/AllBlogPosts";
+
+export default function HomePage() {
+  return (
+    <main>
+      <AllBlogPosts />
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <main>
+      <p>
+        Hi my name is Omar, I’m a software engineer that specializes in creating
+        great front end experiences, primarily using react. When I’m not
+        engineering I like to game and make music. Interested in talking to me
+        about tech or working with me on a project? Reach out to me here.
+      </p>
+
+      <h2>Recent Posts</h2>
+
+      <p>stuff will go here</p>
+    </main>
+  );
+}
