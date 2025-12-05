59565b687ac9b0d3d8eb59aa7ee5fa525ef9735a
Omar Louis White <louiswhite3019@icloud.com>
2025-12-05 16:34:11 -0500

    feat: adds bio component

---
+++ components/Bio.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Bio() {
+  return (
+    <div className="flex flex-col items-center space-y-4">
+      <Image
+        src="/images/omar-headshot.jpg"
+        alt="Omar"
+        width={150}
+        height={150}
+        className="rounded-full"
+      />
+      <p className="text-center max-w-md">
+        Hi my name is Omar, I'm a software engineer that specializes in creating
+        great front end experiences, primarily using react. When I'm not
+        engineering I like to game and make music. Interested in talking to me
+        about tech or working with me on a project? Reach out to me here.
+      </p>
+    </div>
+  );
+}
