c7c082667b2acc6065afe0f0818a78843f7696ee
Omar Louis White <louiswhite3019@icloud.com>
2025-12-05 16:35:28 -0500

    chore: use template string

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -11,10 +11,10 @@ export default function Bio() {
         className="rounded-full"
       />
       <p className="text-center max-w-md">
-        Hi my name is Omar, I'm a software engineer that specializes in creating
+        {`Hi my name is Omar, I'm a software engineer that specializes in creating
         great front end experiences, primarily using react. When I'm not
         engineering I like to game and make music. Interested in talking to me
-        about tech or working with me on a project? Reach out to me here.
+        about tech or working with me on a project? Reach out to me here.`}
       </p>
     </div>
   );
