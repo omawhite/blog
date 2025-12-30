f28777abb5e1c9dba5bc2a6ad8954ebd3b8332ba
Omar Louis White <louiswhite3019@icloud.com>
2025-12-29 21:55:24 -0500

    feat: formats post dates

--- components/blog/PostDate.tsx
+++ components/blog/PostDate.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 interface PostDateProps {
   date: string;
   label?: string;
@@ -7,9 +8,11 @@ export default function PostDate({
   date,
   label = "Published on:",
 }: PostDateProps) {
+  const formattedDate = format(date, "LLLL d, yyyy");
+
   return (
     <p>
-      {label} {date}
+      {label} <time dateTime={date}>{formattedDate}</time>
     </p>
   );
 }
